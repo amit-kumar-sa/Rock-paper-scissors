c993c31abb013634d193369150831bdb402c9458
Amit swain <amitkuswain6@gmail.com>
2024-10-03 21:46:58 +0530

    Rock-Paper-Scissors design part

---
+++ app.js
@@ -0,0 +1,13 @@
+let userScore = 0;
+let compScore = 0;
+
+
+const choices = document.querySelectorAll(".chice");
+
+choices.forEach((choice)=>{
+    
+    choice.addEventListener("click",()=>{
+        console.log("choice was clicked");
+
+    })
+})
